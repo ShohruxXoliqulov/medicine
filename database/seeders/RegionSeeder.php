4b300fe3bae6320aea0605d0dee841fdb708a782
Shohrux <shoxruxxoliqulov314@gmail.com>
2024-02-03 17:46:10 +0500

    import written regions.sql into database

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -2,33 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\Region;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class RegionSeeder extends Seeder
 {
 
-    private $regions = [
-        'Toshkent',
-        'Andijon',
-        'Farg\'ona',
-        'Qashqadaryo',
-        'Surxondaryo',
-        'Jizzax'
-    ];
+
 
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-
-        foreach ($this->regions as $region)
-        {
-            Region::create([
-                'name' => $region,
-            ]);
-        }
+        $regions = File::get(storage_path("app/regions_mysql.sql"));
+        dd($regions);
     }
 }
